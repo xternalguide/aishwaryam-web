f566a4e17cebe3de4cf8149d1db5dd1167292a53
P. Sri Venkatesh <srivenkatesh118@gmail.com>
2026-06-08 14:19:35 +0530

    feat: enable multiple WebView window support for Razorpay payment popups on Android

--- android/app/src/main/java/com/aishwaryam/app/MainActivity.java
+++ android/app/src/main/java/com/aishwaryam/app/MainActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
+import android.os.Message;
+import android.app.Dialog;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.BridgeWebViewClient;
+import com.getcapacitor.BridgeWebChromeClient;
 import android.view.WindowManager;
 import android.graphics.Color;
 
@@ -22,6 +27,11 @@ public class MainActivity extends BridgeActivity {
         WebView webView = this.getBridge().getWebView();
         if (webView != null) {
             webView.setFitsSystemWindows(true);
+            
+            // Enable multiple windows support (required for Razorpay bank authentication/popups)
+            webView.getSettings().setSupportMultipleWindows(true);
+            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            
             webView.setWebViewClient(new BridgeWebViewClient(this.getBridge()) {
                 @Override
                 public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
@@ -40,6 +50,47 @@ public class MainActivity extends BridgeActivity {
                     } else {
                         super.onReceivedError(view, errorCode, description, failingUrl);
                     }
+                }
+            });
+
+            // Set custom WebChromeClient to handle onCreateWindow
+            webView.setWebChromeClient(new BridgeWebChromeClient(this.getBridge()) {
+                @Override
+                public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+                    // Create new WebView instance for the popup
+                    WebView newWebView = new WebView(MainActivity.this);
+                    newWebView.getSettings().setJavaScriptEnabled(true);
+                    newWebView.getSettings().setSupportMultipleWindows(true);
+                    newWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                    
+                    // Create a fullscreen dialog to show the new WebView
+                    final Dialog dialog = new Dialog(MainActivity.this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+                    dialog.setContentView(newWebView);
+                    dialog.show();
+
+                    newWebView.setWebChromeClient(new WebChromeClient() {
+                        @Override
+                        public void onCloseWindow(WebView window) {
+                            dialog.dismiss();
+                        }
+                    });
+                    
+                    newWebView.setWebViewClient(new WebViewClient() {
+                        @Override
+                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                            return false;
+                        }
+                        
+                        @Override
+                        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                            return false;
+                        }
+                    });
+
+                    WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                    transport.setWebView(newWebView);
+                    resultMsg.sendToTarget();
+                    return true;
                 }
             });
         }
